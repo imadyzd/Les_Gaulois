6f1ce31c003950625ecd98f6eb1c9ca7f21c3525
imadyzd <imadeddineyazid@gmail.com>
2023-09-26 00:53:49 +0200

    Fix UTF-8 issues

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -10,13 +10,13 @@ public class Scenario {
 	
 	public static void main(String[] args) {
 		Druide Panoramix = new Druide("Panoramix", 5, 6);
-		Gaulois Obélix = new Gaulois("Obélix", 8);
+		Gaulois Obelix = new Gaulois("Obelix", 8);
 		Gaulois Asterix = new Gaulois("Asterix", 8);
 		Romain Minus = new Romain("Minus", 8);
-		Panoramix.parler("Je vais aller préparer une petite potion...");
+		Panoramix.parler("Je vais aller preparer une petite potion...");
 		Panoramix.preparerPotion();
-		Panoramix.booster(Obélix);
-		Obélix.parler("Par Bélénos, ce n'est pas juste !");
+		Panoramix.booster(Obelix);
+		Obelix.parler("Par Belenos, ce n'est pas juste !");
 		Panoramix.booster(Asterix);
 		Asterix.parler("Bonjour");
 		Asterix.frapper(Minus);
