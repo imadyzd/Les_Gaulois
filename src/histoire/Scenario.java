3c682cb8e10e0540b7b1f3d7e854456524639870
imadyzd <imadeddineyazid@gmail.com>
2023-09-25 20:03:00 +0200

    deuxieme commit

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -1,5 +1,28 @@
 package histoire;
 
+
+
+import personnages.Druide;
+import personnages.Gaulois;
+import personnages.Romain;
+
 public class Scenario {
+	
+	public static void main(String[] args) {
+		Druide Panoramix = new Druide("Panoramix", 5, 6);
+		Gaulois Obélix = new Gaulois("Obélix", 8);
+		Gaulois Asterix = new Gaulois("Asterix", 8);
+		Romain Minus = new Romain("Minus", 8);
+		Panoramix.parler("Je vais aller préparer une petite potion...");
+		Panoramix.preparerPotion();
+		Panoramix.booster(Obélix);
+		Obélix.parler("Par Bélénos, ce n'est pas juste !");
+		Panoramix.booster(Asterix);
+		Asterix.parler("Bonjour");
+		Asterix.frapper(Minus);
+		
+		
+		
+	}
 
 }
--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -23,6 +23,10 @@ public class Gaulois {
 
 		this.force = force;
 	}
+	
+	public String getNom() {
+		return nom;
+	}
 
 
 	public int getEffetPotion() {
@@ -43,7 +47,7 @@ public class Gaulois {
 	public void frapper(Romain romain) {
 
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		romain.recevoirCoup(force / 3);
+		romain.recevoirCoup((force / 3)*effetPotion);
 	}
 
 		
@@ -53,12 +57,17 @@ public class Gaulois {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
 	
+	
+	public void boirePotion(int forcePotion) {
+		effetPotion = forcePotion;
+		parler("Merci Druide, je sens que ma force est " + effetPotion + " fois décuplée.");
+	}
+		
+		
+
 	public static void main(String[] args) {
-		Gaulois asterix = new Gaulois("asterix", 8);
-		System.out.println(asterix);
-		asterix.parler("Bonjour");
-		Romain rom = new Romain("rom", 8);
-		asterix.frapper(rom);
+		
+		
 		
 		
 	}	
